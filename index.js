765eee0c6ed662b7fdeba9f6d347ebe8be2dfebe
ryan-jardine <ryanjardine43@gmail.com>
2024-02-13 15:38:58 -0400

    Setup Server

--- index.js
+++ index.js
@@ -21,4 +21,24 @@ const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
 
+const express = require('express');
+const dotenv = require('dotenv')
 
+const req = express();
+dotenv.config({ path: './.env'}
+
+)
+
+req.set('view engine', 'hbs')
+const path = require("path")
+
+const publicDir = path.join(__dirname, './public')
+
+req.use(express.static(publicDir))
+req.get("/", (req, res) => {
+    res.render("index")
+})
+
+req.listen(5000, ()=> {
+    console.log("server started on port 5000")
+})
