75e9f3b47e9ef4c36d1256c5ab4a17caba667437
ryan-jardine <ryanjardine43@gmail.com>
2024-02-13 21:54:58 -0400

    Fixed Bugs

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app");
 const { getDatabase } = require( "firebase/database");
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 const firebaseConfig = {
     apiKey: "AIzaSyCm8KbBhLf9oyRoXYTuZMDqtlQyO9k7RUg",
     authDomain: "coc-app-c7033.firebaseapp.com",
@@ -17,28 +14,44 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
 
+
+// Initializing the server
 const express = require('express');
 const dotenv = require('dotenv')
+const app = express();
 
-const req = express();
 dotenv.config({ path: './.env'}
 
 )
 
-req.set('view engine', 'hbs')
+app.set('view engine', 'hbs')
+
+// Locations of the files to be served
 const path = require("path")
 
 const publicDir = path.join(__dirname, './public')
 
-req.use(express.static(publicDir))
-req.get("/", (req, res) => {
-    res.render("index")
+app.use(express.static(publicDir))
+
+// Each statement represents a different page to load
+
+// Default to login page
+app.get("/", (req, res) => {
+    res.render("login")
+})
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+app.get("/home", (req, res) => {
+    res.render("home")
 })
 
-req.listen(5000, ()=> {
+app.listen(5000, ()=> {
     console.log("server started on port 5000")
 })
